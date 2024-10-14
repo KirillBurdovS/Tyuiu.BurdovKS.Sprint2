9d46bac1b5d9d624b0fc1400c7b464982a4e89a0
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-10-14 21:35:25 +0300

    Task5

---
+++ Tyuiu.BurdovKS.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,67 @@
+﻿
+
+
+using System.Data;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+
+
+
+namespace Tyuiu.BurdovKS.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            string res;
+
+
+            int dayOfWeek = (k - 1) % 7 + 1;
+
+
+            if ( k <= 0)
+            {
+                return "Некоректный номер дня";
+
+            }
+
+            switch (dayOfWeek)
+            {
+                case 1:
+                    res = "Понедельник ";
+                    break;
+
+                case 2:
+                    res = "Вторник ";
+                    break;
+
+                case 3:
+                    res = "Среда ";
+                    break;
+
+                case 4:
+                    res = "Четверг ";
+                    break;
+
+                case 5:
+                    res = "Пятница ";
+                    break;
+
+                case 6:
+                    res = "Суббота ";
+                    break;
+
+                case 7:
+                    res = "Воскресенье ";
+                    break;
+
+                default:
+                    res = "Неизвестный день ";
+                    break;
+                  
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BurdovKS.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+
+
+using System.ComponentModel.Design;
+using Tyuiu.BurdovKS.Sprint2.Task5.V15.Lib;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+  
+
+
+
+
+
+        Console.Title = "Спринт #2 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+        Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер дня: ");
+
+        int numNedel = Convert.ToInt32(Console.ReadLine());
+
+
+        string res;
+
+
+        if ((numNedel < 1) || (numNedel > 365))
+        {
+            res = "Введенно неверное значение ";
+
+        }
+        else
+        {
+            res = "Этот день: " + ds.FindDayName(numNedel);
+        }
+
+
+        Console.WriteLine(res);
+
+      
+
+
+
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+       
+
+
+
+
+
+    }
+}
