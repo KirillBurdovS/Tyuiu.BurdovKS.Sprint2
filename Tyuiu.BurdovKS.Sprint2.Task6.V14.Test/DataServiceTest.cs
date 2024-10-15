c0e76645396ff4a3cd5ffd47fd343de46526802d
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-10-15 15:24:32 +0300

    Task6

---
+++ Tyuiu.BurdovKS.Sprint2.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+
+
+using Tyuiu.BurdovKS.Sprint2.Task6.V14.Lib;
+
+
+
+namespace Tyuiu.BurdovKS.Sprint2.Task6.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService ds = new DataService();
+
+
+            Assert.AreEqual("Воскресенье", ds.FindDayName(2, 6));
+
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1, 366);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1, 8);
+            });
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.BurdovKS.Sprint2.Task6.V14/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+using Tyuiu.BurdovKS.Sprint2.Task6.V14.Lib;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+
+
+
+
+
+
+        Console.Title = "Спринт #2 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Получение результата из switch                                    *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано целое число k (1 <= k <= 365). Определить, каким днем недели       *");
+        Console.WriteLine("* является k-й день не високосного года,                                  *");
+        Console.WriteLine("* в котором 1 января d-й день недели                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите число от 1 до 365:");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите число от 1 до 7:");
+        int d = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        try
+        {
+            string day = ds.FindDayName(k, d);
+            Console.WriteLine(day);
+        }
+        catch
+        {
+            Console.WriteLine("Неверно");
+        }
+        Console.ReadKey();
+
+        int res;
+    }
+}
+
+
+
+
